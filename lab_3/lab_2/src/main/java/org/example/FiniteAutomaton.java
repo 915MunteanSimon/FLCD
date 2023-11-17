60ec0e820fd01dc84d4c9e83a0f2d414aa7e497e
Muntean Simon <91518238+915MunteanSimon@users.noreply.github.com>
2023-11-17 16:43:21 +0200

    Create FiniteAutomaton.java

---
+++ lab_3/lab_2/src/main/java/org/example/FiniteAutomaton.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class FiniteAutomaton {
+
+  private void readFromFile(string filePath){
+    try(Scanner scanner = new Scanner)
+
+}
+
+}
